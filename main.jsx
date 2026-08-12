836169ab2f1476922d1ed5bb8243c452d21ed2bc
CapitalReno <service@capitalreno.net>
2026-08-12 15:46:48 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CRCDashboard from './CRCDashboard.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CRCDashboard />
+  </React.StrictMode>
+);
